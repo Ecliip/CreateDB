53eea592d9cafd2ac6cb79024ace6546ff33a305
Mika <prodomitor@gmail.com>
2020-07-05 17:38:06 +0200

    the project is initialized

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Here we will manipulate some data</h1>
+
+<h3>select below what action you want to perform</h3>
+<h4 class="create">Create</h4>
+<h4 class="create">Read</h4>
+<h4 class="create">Update</h4>
+<h4 class="create">Delete</h4>
+</body>
+</html>
